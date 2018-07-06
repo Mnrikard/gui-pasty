31b08c19381f00b804edfe7ea52556f363be8e3a
Mnrikard <mnrikard@gmail.com>
2018-07-06 13:49:40 -0400

    refactored to stop compiler errors

--- renderer.js
+++ renderer.js
@@ -7,7 +7,7 @@ var d = document;
 
 var gpFunc = d.getElementById('gp-func');
 var gpParms = d.getElementById('gp-parms');
-var log = document.getElementById('log');
+var log = d.getElementById('log');
 
 var editors = require("pasty-clipboard-editor/editors");
 var names = editors.getAllFuncNames();
@@ -20,171 +20,39 @@ for(i=0;i<names.length;i++){
 	gpFunc.appendChild(opt);
 }
 
+function editClipboard(parms){
+	try{
+		var content = "";
+		const {clipboard} = require('electron')
+		try{
+			content = clipboard.readText();
+		} catch(e) {
+			clipboard.writeText(content);
+		}
+		var newContent = require('pasty-clipboard-editor/editorRunner').handleInput(content, parms);
+		clipboard.writeText(newContent);
+	} catch(err) {
+		console.log(err);
+	}
+
+}
+
+function getRecentFuncs(){
+	var recentCalls = localStorage.getItem("recentPastyFunctions");
+	if(!recentCalls){
+		return [];
+	}
+	return JSON.parse(recentCalls);
+}
+
 function clearParmArea(){
 	while(gpParms.childNodes[0]){
 		gpParms.removeChild(gpParms.childNodes[0]);
 	}
 }
 
-exports.functionChosen = function(){
-	var i;
-	var fn = editors.getEditor(getSelectedFunction(), false);
-	fn.calledName = getSelectedFunction();
-	if (fn.getParms){
-		fn.getParms();
-	}
-	if(fn.updateHelpText){fn.updateHelpText();}
-
-	clearParmArea();
-
-	gpParms.innerText = fn.oneLiner;
-
-	var tb = document.createElement("table");
-	gpParms.appendChild(tb);
-
-	if(fn.parms){
-		for(i=0;i<fn.parms.length;i++){
-			var tr = document.createElement("tr");
-			tb.appendChild(tr);
-			var lbl = document.createElement("td");
-			lbl.className = "label";
-			lbl.appendChild(document.createTextNode(fn.parms[i].name));
-			tr.appendChild(lbl);
-			var inpCell = document.createElement("td");
-			var inp = document.createElement("input");
-			inp.type="text";
-			inp.id = "gpParm"+i;
-			inp.className = "functionParameter";
-			if(fn.parms[i].defaultValue){
-				inp.value = fn.parms[i].defaultValue.replace(/\n/,"\\n").replace(/\t/,"\\t");
-			}
-			inpCell.appendChild(inp);
-			tr.appendChild(inpCell);
-		}
-	}
-
-	var btn = document.createElement("button");
-	btn.appendChild(document.createTextNode("Run"));
-	btn.className = "round";
-	btn.id = "runButton";
-	gpParms.appendChild(btn);
-	btn.addEventListener("click", runFunction);
-}
-
 function getSelectedFunction(){
 	return gpFunc.value;
-}
-
-function editClipboard(parms){
-	try{
-		var content = "";
-		const {clipboard} = require('electron')
-		try{
-			content = clipboard.readText();
-		} catch(e) {
-			clipboard.writeText(content);
-		}
-		var newContent = require('pasty-clipboard-editor/editorRunner').handleInput(content, parms);
-		clipboard.writeText(newContent);
-	} catch(err) {
-		console.log(err);
-	}
-
-}
-
-function getRecentFuncs(){
-	var recentCalls = localStorage.getItem("recentPastyFunctions");
-	if(!recentCalls){
-		return [];
-	}
-	return JSON.parse(recentCalls);
-}
-
-Array.prototype.pluck = function(i){
-	return this.splice(i,1);
-};
-
-Array.prototype.equals = function(arr2){
-	if(this.length !== arr2.length) { return false; }
-	for(var p=0;p<this.length;p++){
-		if(this[p] !== arr2[p]){
-			return false;
-		}
-	}
-	return true;
-};
-
-	var i;
-	var fn = editors.getEditor(getSelectedFunction(), false);
-	fn.calledName = getSelectedFunction();
-	if (fn.getParms){
-		fn.getParms();
-	}
-	if(fn.updateHelpText){fn.updateHelpText();}
-
-	clearParmArea();
-
-	gpParms.innerText = fn.oneLiner;
-
-	var tb = document.createElement("table");
-	gpParms.appendChild(tb);
-
-	if(fn.parms){
-		for(i=0;i<fn.parms.length;i++){
-			var tr = document.createElement("tr");
-			tb.appendChild(tr);
-			var lbl = document.createElement("td");
-			lbl.className = "label";
-			lbl.appendChild(document.createTextNode(fn.parms[i].name));
-			tr.appendChild(lbl);
-			var inpCell = document.createElement("td");
-			var inp = document.createElement("input");
-			inp.type="text";
-			inp.id = "gpParm"+i;
-			inp.className = "functionParameter";
-			if(fn.parms[i].defaultValue){
-				inp.value = fn.parms[i].defaultValue.replace(/\n/,"\\n").replace(/\t/,"\\t");
-			}
-			inpCell.appendChild(inp);
-			tr.appendChild(inpCell);
-		}
-	}
-
-	var btn = document.createElement("button");
-	btn.appendChild(document.createTextNode("Run"));
-	btn.className = "round";
-	btn.id = "runButton";
-	gpParms.appendChild(btn);
-	btn.addEventListener("click", runFunction);
-}
-
-function getSelectedFunction(){
-	return gpFunc.value;
-}
-
-function editClipboard(parms){
-	try{
-		var content = "";
-		const {clipboard} = require('electron')
-		try{
-			content = clipboard.readText();
-		} catch(e) {
-			clipboard.writeText(content);
-		}
-		var newContent = require('pasty-clipboard-editor/editorRunner').handleInput(content, parms);
-		clipboard.writeText(newContent);
-	} catch(err) {
-		console.log(err);
-	}
-
-}
-
-function getRecentFuncs(){
-	var recentCalls = localStorage.getItem("recentPastyFunctions");
-	if(!recentCalls){
-		return [];
-	}
-	return JSON.parse(recentCalls);
 }
 
 Array.prototype.pluck = function(i){
@@ -224,8 +92,8 @@ function runFunction(){
 		parms.push(getSelectedFunction());
 		var i=0;
 
-		while(document.getElementById("gpParm"+i)){
-			parms.push(document.getElementById("gpParm"+i).value);
+		while(d.getElementById("gpParm"+i)){
+			parms.push(d.getElementById("gpParm"+i).value);
 			i++;
 		}
 		rememberFunction(parms);
@@ -238,6 +106,80 @@ function runFunction(){
 
 	document.getElementById('gp-func').select();
 	exports.showPastFunctions();
+}
+
+exports.functionChosen = function(){
+	var i,tr,lbl,inpCell,inp;
+	var fn = editors.getEditor(getSelectedFunction(), false);
+	fn.calledName = getSelectedFunction();
+	if (fn.getParms){
+		fn.getParms();
+	}
+	if(fn.updateHelpText){fn.updateHelpText();}
+
+	clearParmArea();
+
+	gpParms.innerText = fn.oneLiner;
+
+	var tb = d.createElement("table");
+	gpParms.appendChild(tb);
+
+	if(fn.parms){
+		for(i=0;i<fn.parms.length;i++){
+			tr = d.createElement("tr");
+			tb.appendChild(tr);
+			lbl = d.createElement("td");
+			lbl.className = "label";
+			lbl.appendChild(d.createTextNode(fn.parms[i].name));
+			tr.appendChild(lbl);
+			inpCell = d.createElement("td");
+			inp = d.createElement("input");
+			inp.type="text";
+			inp.id = "gpParm"+i;
+			inp.className = "functionParameter";
+			if(fn.parms[i].defaultValue){
+				inp.value = fn.parms[i].defaultValue.replace(/\n/,"\\n").replace(/\t/,"\\t");
+			}
+			inpCell.appendChild(inp);
+			tr.appendChild(inpCell);
+		}
+	}
+
+	var btn = d.createElement("button");
+	btn.appendChild(d.createTextNode("Run"));
+	btn.className = "round";
+	btn.id = "runButton";
+	gpParms.appendChild(btn);
+	btn.addEventListener("click", runFunction);
+}
+
+function getSelectedFunction(){
+	return gpFunc.value;
+}
+
+function editClipboard(parms){
+	try{
+		var content = "";
+		const {clipboard} = require('electron')
+		try{
+			content = clipboard.readText();
+		} catch(e) {
+			clipboard.writeText(content);
+		}
+		var newContent = require('pasty-clipboard-editor/editorRunner').handleInput(content, parms);
+		clipboard.writeText(newContent);
+	} catch(err) {
+		console.log(err);
+	}
+
+}
+
+function getRecentFuncs(){
+	var recentCalls = localStorage.getItem("recentPastyFunctions");
+	if(!recentCalls){
+		return [];
+	}
+	return JSON.parse(recentCalls);
 }
 
 exports.showPastFunctions = function(){
